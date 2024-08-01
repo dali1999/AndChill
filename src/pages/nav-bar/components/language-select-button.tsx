f51970d8b463dfd93143e50cfd3d8954c89c58c1
dali1999 <dali1999@naver.com>
2024-08-01 17:05:44 +0900

    fix: 쓰이지 않는 선언 예외처리

--- src/pages/nav-bar/components/language-select-button.tsx
+++ src/pages/nav-bar/components/language-select-button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import { useRegionConfigQuery } from '@hooks/react-query/use-query-config';
