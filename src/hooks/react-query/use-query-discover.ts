fc5ed927f24566fc1c615aca4f1d60556eac3d4a
dali1999 <dali1999@naver.com>
2024-08-07 13:50:28 +0900

    feat: '영화 탐색' query 파라미터에 sortBy 추가

--- src/hooks/react-query/use-query-discover.ts
+++ src/hooks/react-query/use-query-discover.ts
@@ -3,10 +3,10 @@ import { TMovieListsFetchRes } from '@api/movie-lists/movie-lists-request.type';
 import { QUERY_KEY } from '@constants/query-key';
 import { useQuery } from '@tanstack/react-query';
 
-export const useMovieDiscoverResultsQuery = (genres: string) => {
+export const useMovieDiscoverResultsQuery = (sortBy: string, genres: string) => {
   const query = useQuery<TMovieListsFetchRes, Error>({
     queryKey: [QUERY_KEY.movieDiscoveredResults, genres],
-    queryFn: async () => await discoverRequest.fetchMovieDiscoverResults(genres),
+    queryFn: async () => await discoverRequest.fetchMovieDiscoverResults(sortBy, genres),
   });
   return query;
 };
