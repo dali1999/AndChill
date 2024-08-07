6f3dda85da93e7bc0904c2837ae1386f8037ea5b
dali1999 <dali1999@naver.com>
2024-08-07 13:50:53 +0900

    feat: 홈 화면 '장르별 영화 목록' 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,41 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
+import { useMovieDiscoverResultsQuery } from '@hooks/react-query/use-query-discover';
 import { useTrendingMovieListQuery } from '@hooks/react-query/use-query-trending';
 import MovieList from '@pages/home/components/trending-movie-list/movie-list';
 import UpcomingMovieList from '@pages/home/components/upcoming-movie-list/upcoming-movie-list';
 import styled from 'styled-components';
+// original_title.desc
+// popularity.desc
+// revenue.desc
+// primary_release_date.desc
+// title.desc
+// vote_average.desc
+// vote_count.desc
 
 const Home = () => {
-  const { data: trendingMovieData, isLoading, isError } = useTrendingMovieListQuery();
+  const { data: trendingMovieData, isLoading: isTrendingLoading } = useTrendingMovieListQuery();
+  const sortByVotes = 'vote_count.desc';
+  const horrorGenres = [27, 53].join();
+  const animationGenres = [16].join();
+  const westGenres = [37].join();
+  const { data: discoveredMovieData, isLoading: isHorrorLoading } = useMovieDiscoverResultsQuery(
+    sortByVotes,
+    horrorGenres,
+  );
+  const { data: animationMovieData, isLoading: isAnimationLoading } = useMovieDiscoverResultsQuery(
+    sortByVotes,
+    animationGenres,
+  );
+  const { data: westMovieData, isLoading: isWestLoading } = useMovieDiscoverResultsQuery(sortByVotes, westGenres);
 
   return (
     <S.Container>
       <UpcomingMovieList />
-      <MovieList title="이번 주 🌎 트렌드" data={trendingMovieData} isLoading={isLoading} />
+      <MovieList title="이번 주 🌎 트렌드" data={trendingMovieData} isLoading={isTrendingLoading} />
+      <MovieList title="공포, 스릴러" data={discoveredMovieData} isLoading={isHorrorLoading} />
+      <MovieList title="애니메이션" data={animationMovieData} isLoading={isAnimationLoading} />
+      <MovieList title="서부" data={westMovieData} isLoading={isWestLoading} />
     </S.Container>
   );
 };
