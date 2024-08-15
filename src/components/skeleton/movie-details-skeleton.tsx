04b4aaff119211121a434e1d9d85883d89dabe99
dali1999 <dali1999@naver.com>
2024-08-16 02:44:04 +0900

    feat: 영화 세부정보 skeleton 추가

---
+++ src/components/skeleton/movie-details-skeleton.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const MovieDetailsSkeleton = () => {
+  return (
+    <S.container>
+      <S.DummyContent></S.DummyContent>
+    </S.container>
+  );
+};
+
+export default MovieDetailsSkeleton;
+
+const S = {
+  container: styled.div`
+    overflow: hidden;
+    background-color: var(--indigo02);
+    border-radius: 20px 20px 0 0;
+    width: 100%;
+    max-width: 1500px;
+    height: 1200px;
+    box-shadow: rgb(0, 0, 0) 0px 20px 80px -10px;
+    margin-top: 300px;
+  `,
+  DummyContent: styled.div``,
+};
